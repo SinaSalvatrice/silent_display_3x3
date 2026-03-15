358b2ecdcff78dbdef64b86bef09646679749847
SinaSalvatrice <sinagessner.wuppertal@gmail.com>
2026-03-15 19:53:05 +0100

    Update silent_display_3x3.h

--- silent_display_3x3.h
+++ silent_display_3x3.h
@@ -1,11 +1,2 @@
-
-#pragma once
-#include "../quantum/quantum.h"
-
-// silent_display_3x3.h
-#ifndef SILENT_DISPLAY_3X3_H
-#define SILENT_DISPLAY_3X3_H
-
-// Add your function declarations and macros here
-
-#endif // SILENT_DISPLAY_3X3_H
+// keyboards/silent_display_3x3/silent_display_3x3.h
+// Add your header content here
